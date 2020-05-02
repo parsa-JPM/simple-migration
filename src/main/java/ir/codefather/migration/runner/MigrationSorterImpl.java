b997b7301e868c10088a5a03ebd4c4f844be1f85
parsa-script <mihandoost.p@gmail.com>
2020-05-02 20:58:39 +0430

    fix import classes

--- src/main/java/ir/codefather/migration/runner/MigrationSorterImpl.java
+++ src/main/java/ir/codefather/migration/runner/MigrationSorterImpl.java
@@ -1,9 +1,7 @@
 package ir.codefather.migration.runner;
 
-import java.text.Collator;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
